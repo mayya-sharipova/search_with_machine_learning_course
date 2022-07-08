d6f1da671fedd76978f9f9cf56cb639b9a5bf3e9
Mayya Sharipova <mayya.sharipova@elastic.co>
2022-07-08 01:59:01 +0000

    week 3 project

--- utilities/query.py
+++ utilities/query.py
@@ -11,11 +11,15 @@ from urllib.parse import urljoin
 import pandas as pd
 import fileinput
 import logging
+import fasttext
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logging.basicConfig(format='%(levelname)s:%(message)s')
+
+model_file = r'/workspace/datasets/fasttext/cat_training/query_cat_classifier.bin'
+model = fasttext.load_model(model_file)
 
 # expects clicks and impressions to be in the row
 def create_prior_queries_from_group(
@@ -188,9 +192,38 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
 
 def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
     #### W3: classify the query
+    (ctgs, scores) = model.predict(user_query, k=5)
+    print("Predicted categories with scores", ctgs, scores)
+    
     #### W3: create filters and boosts
+    filters = None
+    threshold = 0.5
+    if len(ctgs) > 0:
+        if len(ctgs) == 1 and scores[0] >= threshold:
+            ctg = ctgs[0][9:]
+            filters = []
+            filters.append({
+                "term" : { "categoryPathIds.keyword" : ctg }
+            })
+        elif sum(scores) >= threshold:
+            cur_sum = 0
+            top_ctgs = []
+            i = 0
+            while i < len(scores):
+                top_ctgs.append(ctgs[i][9:])
+                cur_sum += scores[i]
+                i += 1
+                if cur_sum >= threshold:
+                    break
+            filters = []
+            filters.append({
+                "terms" : { "categoryPathIds.keyword" : top_ctgs }
+            })
+    print("Filters", filters)
+        
+
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    query_obj = create_query(user_query, click_prior_query=None, filters=filters, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
--- week3/create_labeled_queries.py
+++ week3/create_labeled_queries.py
@@ -4,9 +4,20 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+import re
 
 # Useful if you want to perform stemming.
 import nltk
+
+
+def get_parent_cat_with_min_counts(cat, categories_by_counts, parents_df):
+    if cat in categories_by_counts:
+        return cat
+    
+    parent_ctg = parents_df.loc[parents_df['category'] == cat, 'parent'].iloc[0]
+    get_parent_cat_with_min_counts(parent_ctg, categories_by_counts, parents_df)
+
+
 stemmer = nltk.stem.PorterStemmer()
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
@@ -49,8 +60,43 @@ df = pd.read_csv(queries_file_name)[['category', 'query']]
 df = df[df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+non_alpahnum_pattern = re.compile('[^a-zA-Z0-9]')
+multiple_spaces_pattern = re.compile('\s+')
+categories_by_counts = {}
+for index, row in df.iterrows():
+    query = row['query']
+    # substitute non-alphanumeric characters with spaces
+    normalized_query = re.sub(non_alpahnum_pattern, ' ', query)
+    # substitue multiple spaces with a single spac
+    normalized_query = re.sub(multiple_spaces_pattern, ' ', normalized_query)
+    # lower case and stem, PorterStemmer also lowercases
+    stemmed_query = stemmer.stem(normalized_query)
+    df.at[index,'query'] = stemmed_query
+    
+    category = row['category']
+    categories_by_counts[category] = categories_by_counts.get(category, 0) + 1
+   
+
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+is_rolled = True
+while is_rolled == True:
+    is_rolled = False
+    new_categories_by_counts = {}
+    for ctg, cnt in categories_by_counts.items():
+        new_ctg = ctg
+        if cnt < min_queries:
+            if ctg in parents_df['category'].unique():
+                parent_ctg = parents_df.loc[parents_df['category'] == ctg, 'parent'].iloc[0]
+                new_ctg = parent_ctg
+                is_rolled = True
+            else:
+                print(ctg)
+        new_categories_by_counts[new_ctg] = new_categories_by_counts.get(new_ctg, 0) + cnt
+    print(len(categories_by_counts), '->', len(new_categories_by_counts))          
+    categories_by_counts = new_categories_by_counts
+    
+df['category']= df['category'].apply(lambda cat: get_parent_cat_with_min_counts(cat, categories_by_counts, parents_df))
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
